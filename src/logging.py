016b2cb621795e0d96e62610b735b6dd36fcbfe8
MinecraftLetsPlay <dennisplischke755@gmail.com>
2025-01-22 14:06:51 +0100

    Ready to run

--- src/logging.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-logging.basicConfig(filename='bot_01.log',
-                    level=logging.DEBUG,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
-
-logging.info("This is logged to a file.")
