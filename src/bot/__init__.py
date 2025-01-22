823c32011421c9ed3cdd30a9ef662e7793cff47d
olittlefoxE <little.fox.e1000@gmail.com>
2025-01-22 13:47:19 +0100

    added embedded messages for events and responses

---
+++ src/bot/__init__.py
@@ -0,0 +1,4 @@
+# __init__.py
+
+# This file is used to initialize the bot module. It can be used to import necessary submodules,
+# set up configurations, or perform any other initialization tasks required for the module.
--- src/responses.py
+++ src/responses.py
@@ -1,15 +1,30 @@
 import random
+import discord
 
-def get_response(message: str) -> str:
+def get_response(message: str) -> discord.Embed:
     p_message = message.lower()
 
     if p_message == '!help':
-        return 'Possible Commands: [System]: !shutdown, !restart [Public]: !roll, !test.'
+        embed = discord.Embed(title="Help", description="Possible Commands", color=0x00ff00)
+        embed.add_field(name="[System]", value="!shutdown, !restart", inline=False)
+        embed.add_field(name="[Public]", value="!roll, !test, !info, !ping", inline=False)
+        return embed
 
     if p_message == '!test':
-        return 'Hello! I am online and ready...'
+        embed = discord.Embed(title="Test", description="Hello! I am online and ready...", color=0x00ff00)
+        return embed
 
     if p_message == '!roll':
-        return str(random.randint(1, 6))
+        embed = discord.Embed(title="Roll", description=str(random.randint(1, 6)), color=0x00ff00)
+        return embed
 
-    return 'I do not understand that command.'
+    if p_message == '!info':
+        embed = discord.Embed(title="Info", description="This is a Discord bot created for demonstration purposes.", color=0x00ff00)
+        return embed
+
+    if p_message == '!ping':
+        embed = discord.Embed(title="Ping", description="Pong!", color=0x00ff00)
+        return embed
+
+    embed = discord.Embed(title="Error", description="I do not understand that command.", color=0xff0000)
+    return embed
