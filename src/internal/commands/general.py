b12684e2aebc79e6e6c3fb5522f573d6f317f99a
olittlefoxE <little.fox.e1000@gmail.com>
2025-01-22 15:13:38 +0100

    changed function output from none to out

--- src/internal/commands/general.py
+++ src/internal/commands/general.py
@@ -1,10 +1,10 @@
 # src/internal/commands/general.py
 import discord
 
-async def info(client, message) -> None:
+async def info(client, message):
     embed = discord.Embed(title="Info", description="This is a Discord bot created for demonstration purposes.", color=0x00ff00)
     await message.channel.send(embed=embed)
 
-async def ping(client, message) -> None:
+async def ping(client, message):
     embed = discord.Embed(title="Ping", description="Pong!", color=0x00ff00)
     await message.channel.send(embed=embed)
