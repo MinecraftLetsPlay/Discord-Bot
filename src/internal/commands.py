987566ad68e449c6e156efeabd28a955fbb9555c
MinecraftLetsPlay <dennisplischke755@gmail.com>
2025-01-22 17:49:35 +0100

    Corrected username

--- src/internal/commands.py
+++ src/internal/commands.py
@@ -40,5 +40,5 @@ async def handle_command(client, message):
 def is_authorized(user):
     """Checks if the user is authorized for system commands."""
     # Replace with your authorized user logic
-    authorized_users = ["minecraftletsplay#0"]
+    authorized_users = ["minecraftletsplay2912#0"]
     return str(user) in authorized_users
