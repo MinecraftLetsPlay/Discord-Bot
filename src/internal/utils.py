81fa0eb7d3a5d38e3928cdf5f9fcc1a6fbcaa631
Minecraft Lets Play <113841482+MinecraftLetsPlay@users.noreply.github.com>
2025-01-22 15:25:59 +0100

    Delete src directory

--- src/internal/utils.py
+++
@@ -1,5 +0,0 @@
-import json
-
-def load_config(path='src/config/config.json'):
-    with open(path, 'r') as file:
-        return json.load(file)
--- src/main.py
+++
@@ -1,5 +0,0 @@
-# src/main.py
-import internal.bot as bot
-# This file is the main entry point for the bot
-if __name__ == '__main__':
-    bot.run_discord_bot()
