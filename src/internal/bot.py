6c43a63f610bfc9a490ada5473794ec4d91026c9
olittlefoxE <little.fox.e1000@gmail.com>
2025-01-22 15:09:46 +0100

    get token from config.json

--- src/internal/bot.py
+++ src/internal/bot.py
@@ -2,15 +2,12 @@
 import discord
 import os
 import sys
-import responses
-from internal import events, utils, commands
+from internal import events, utils, responses, commands
+from internal.commands import general, system
 
 def run_discord_bot():
     config = utils.load_config()
-    TOKEN = os.getenv('DISCORD_BOT_TOKEN')
-
-    if not TOKEN:
-        raise ValueError("No DISCORD_BOT_TOKEN found in environment variables")
+    TOKEN = config['token']
 
     intents = discord.Intents.default()
     intents.message_content = True
@@ -25,6 +22,6 @@ def run_discord_bot():
 
     @client.event
     async def on_message(message):
-        await events.on_message(client, message, responses, commands, sys, os)
+        await events.on_message(client, message, responses, sys, os)
 
     client.run(TOKEN)
--- src/internal/commands/general.py
+++ src/internal/commands/general.py
@@ -1,10 +1,10 @@
 # src/internal/commands/general.py
 import discord
 
-async def info(client, message):
+async def info(client, message) -> None:
     embed = discord.Embed(title="Info", description="This is a Discord bot created for demonstration purposes.", color=0x00ff00)
     await message.channel.send(embed=embed)
 
-async def ping(client, message):
+async def ping(client, message) -> None:
     embed = discord.Embed(title="Ping", description="Pong!", color=0x00ff00)
     await message.channel.send(embed=embed)
