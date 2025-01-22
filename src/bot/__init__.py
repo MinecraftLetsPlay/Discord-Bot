6cde5bd44f3780a73e8418885efbaf2998c56fb2
MinecraftLetsPlay <dennisplischke755@gmail.com>
2025-01-22 14:05:48 +0100

    Deleted bot folder

--- src/bot/__init__.py
+++
@@ -1,4 +0,0 @@
-# __init__.py
-
-# This file is used to initialize the bot module. It can be used to import necessary submodules,
-# set up configurations, or perform any other initialization tasks required for the module.
