9aa4e45415ab8a9aa056c6041dd0761a087c6500
MinecraftLetsPlay <dennisplischke755@gmail.com>
2025-01-24 21:11:25 +0100

    Added some public commands

--- src/internal/commands.py
+++ src/internal/commands.py
@@ -34,7 +34,7 @@ async def handle_command(client, message):
     if user_message == '!help':
         embed = discord.Embed(title="Help", description="Possible Commands", color=0x00ff00)
         embed.add_field(name="[System]", value="!shutdown, !restart", inline=False)
-        embed.add_field(name="[Public]", value="!roll, !info, !ping", inline=False)
+        embed.add_field(name="[Public]", value="!help, !info, !rules", inline=False)
         embed.add_field(name="[Moderation]", value="!kick, !ban, !unban, !timeout, !untimeout", inline=False)
         await message.channel.send(embed=embed)
         
@@ -46,9 +46,33 @@ async def handle_command(client, message):
         embed.add_field(name="", value="Planned features will include: Moderation, different utilities, minigames and more.", inline=False)
         await message.channel.send(embed=embed)
         
-    # !roll command
-    if user_message == '!roll':
-        return str(random.randint(1, 6))
+    # !rules command
+    if user_message == '!rules':
+        rules_channel = discord.utils.get(message.guild.text_channels, name="rules")  # Replace "rules" with the actual name of your rules channel
+        if rules_channel:
+            await message.channel.send(f"Please read the rules here: {rules_channel.mention}")
+        else:
+            await message.channel.send("Sorry, I couldn't find a channel named 'rules'.")
+            
+    if user_message.startswith('!userinfo'):
+        if len(message.mentions) > 0:
+            user = message.mentions[0]
+            embed = discord.Embed(title=f"User Info: {user.name}", color=discord.Color.blue())
+            embed.add_field(name="Joined at", value=user.joined_at.strftime("%B %d, %Y"))
+            embed.add_field(name="Roles", value=", ".join([role.name for role in user.roles if role.name != "@everyone"]))
+            embed.set_thumbnail(url=user.avatar.url)
+            await message.channel.send(embed=embed)
+        else:
+            await message.channel.send("Please mention a valid user.")
+            
+    if user_message == '!serverinfo':
+        server = message.guild
+        embed = discord.Embed(title=f"Server Info: {server.name}", color=discord.Color.green())
+        embed.add_field(name="Server Created", value=server.created_at.strftime("%B %d, %Y"))
+        embed.add_field(name="Member Count", value=server.member_count)
+        embed.add_field(name="Region", value=server.region)
+        embed.set_thumbnail(url=server.icon.url)
+        await message.channel.send(embed=embed)
     
     #
     #
