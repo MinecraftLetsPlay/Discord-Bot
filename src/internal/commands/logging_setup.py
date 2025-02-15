0d0b43f47ebb8a89023f75f57d7719eff2c4f592
MinecraftLetsPlay <dennisplischke755@gmail.com>
2025-02-15 14:08:38 +0100

    refined

--- src/internal/commands/logging_setup.py
+++ src/internal/commands/logging_setup.py
@@ -1,12 +1,12 @@
 import logging
 import os
 import sys
+from datetime import datetime
 from internal import utils
 
 # Load config
 config = utils.load_config()
 log_directory = config.get("log_file_location")
-print (log_directory)
 
 # Ensure log directory exists
 if not os.path.exists(log_directory):
@@ -15,22 +15,6 @@ if not os.path.exists(log_directory):
     except PermissionError as e:
         print(f"Permission denied: {e}")
         sys.exit(1)
-
-# Setup logging
-log_file = os.path.normpath(os.path.join(log_directory, 'log01.txt'))
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-    handlers=[
-        logging.FileHandler(log_file, encoding="utf-8"),
-        logging.StreamHandler(sys.stdout),
-    ]
-)
-
-# Discord-Logger auf WARNING setzen, damit er nicht in "bot.log" schreibt
-discord_logger = logging.getLogger("discord")
-discord_logger.setLevel(logging.WARNING)
 
 # Function to rotate logs
 def rotate_logs():
@@ -41,3 +25,21 @@ def rotate_logs():
             os.rename(os.path.join(log_directory, log_file), os.path.join(log_directory, f'log{i:02d}.txt'))
 
 rotate_logs()
+
+# Create a new log file with a timestamp
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+log_file = os.path.normpath(os.path.join(log_directory, f'log_{timestamp}.txt'))
+
+# Setup logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler(log_file, encoding="utf-8"),
+        logging.StreamHandler(sys.stdout),
+    ]
+)
+
+# Set Discord logger to WARNING to prevent it from logging to "bot.log"
+discord_logger = logging.getLogger("discord")
+discord_logger.setLevel(logging.WARNING)
