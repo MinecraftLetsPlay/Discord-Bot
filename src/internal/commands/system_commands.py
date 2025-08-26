ea0ed42f167ff4188f822fc3a8c553627e9484c2
Minecraft Lets Play <113841482+MinecraftLetsPlay@users.noreply.github.com>
2025-08-27 00:01:16 +0200

    Removed irrevelvant comments

--- src/internal/commands/system_commands.py
+++ src/internal/commands/system_commands.py
@@ -127,7 +127,6 @@ def setup_system_commands(bot):
         if is_authorized(interaction.user):
             await interaction.response.send_message("Shutting down the bot...")
             log_.info(f"System: Shutdown command executed by: {interaction.user}")
-            # discord.status = discord.Status.offline  # Entfernen, gibt es nicht!
             await bot.close()
         else:
             embed = discord.Embed(
@@ -152,7 +151,6 @@ def setup_system_commands(bot):
                 await bot.wait_for("reaction_add", timeout=30.0, check=check)
                 await interaction.response.send_message("Shutting down the bot and the Raspberry Pi...")
                 log_.info(f"System: Full shutdown command executed by: {interaction.user}")
-                # discord.status = discord.Status.offline  # Entfernen, gibt es nicht!
                 await bot.close()
                 os.system("sudo shutdown now")
             except asyncio.TimeoutError:
